41ec7ede5c63307d853e4696d8d5b4809e018385
Steffan D. Klausen <sak.sakelig@gmail.com>
2021-05-24 23:33:49 +0200

    [Ansatte] Made sort functions for all the fields
    (With help/tip from Syvert and Marie) :)

--- Ansatte/ansatte.js
+++ Ansatte/ansatte.js
@@ -59,10 +59,72 @@ document.querySelector('#searchBtn').addEventListener('click', function() {
 
     searchResultDiv.innerHTML = htmlAnsattTxt;
 
-}); // Slutten av knappen
+    console.log(ansattArray);
+    
+    //Array med ansatte basert på STILLING
+    var ansattStillingArray = [];
+    
+    stillingsFilter(ansattArray);
+    function stillingsFilter(Array){
+      for(var i = 0; i < Array.length; i++){
+        console.log(Array.length);
+        console.log(Array[i].position);
+        console.log(stillingsNavn.options[stillingsNavn.selectedIndex].text);
+        if(Array[i].position.includes(stillingsNavn.options[stillingsNavn.selectedIndex].text) || "Alle Stillinger" === stillingsNavn.options[stillingsNavn.selectedIndex].text){
+          
+          ansattStillingArray.push(Array[i]);
+        }
+      }
+    }
+    console.log(ansattStillingArray);
+    
+
+    var ansattAvdelingsArray = [];
+
+    avdelingsFilter(ansattStillingArray);
+    function avdelingsFilter(Array){
+      for(var i = 0; i < ansattStillingArray.length; i++){
+        if(Array[i].restaurant.includes(avdelingsNavn.options[avdelingsNavn.selectedIndex].text) || "Alle Avdelinger" === avdelingsNavn.options[avdelingsNavn.selectedIndex].text){
+          
+          ansattAvdelingsArray.push(Array[i]);
+        }
+      }
+    }
+    console.log(ansattAvdelingsArray);
+
+    var ansattNavnArray = [];
+    
+    navnFilter(ansattAvdelingsArray)
+    function navnFilter(Array){
+      for(var i = 0; i < ansattAvdelingsArray.length; i++){
+        if(Array[i].name.toLowerCase().includes(ansattNavn.value.toLowerCase()) ){
+    
+          ansattNavnArray.push(Array[i]);
+        }
+      }
+    }
+
+    console.log(ansattNavnArray);
+
+    // ansattNavnArray inneholder filtrert resultat basert på alle feltene!!!
+
+    //For å resete arrays
+    ansattStillingArray = [];
+    ansattAvdelingsArray = [];
+    ansattNavnArray = [];
+  }); // Slutten av knappen
+  
+  // Array med alle ansatte
+  var ansattArray = AnsattModule.getAllEmployees();
+
+
+
+
+
+
+
 
 //funksjon for å legge til ansatte basert på Avdeling
-
 function addAnsatteMedAvdeling(){
     AnsattModule.getEmployeeByRestaurant(avdelingsNavn.options[avdelingsNavn.selectedIndex].text).forEach(ansatte =>{
           
@@ -109,7 +171,7 @@ function addAnsatteMedAvdeling(){
   })
 }
 
-//////
+
 
 
 
