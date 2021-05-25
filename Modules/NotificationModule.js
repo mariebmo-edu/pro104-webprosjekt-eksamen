02d9a2cb36e430c754193ef698bfe0111ff1f2ac
mariestigen <marie.stigen@hotmail.com>
2021-05-25 14:57:58 +0200

    Made restaurant page dynamic
    No need to have multiple pages, it gets the info from the selected module and updates the page based on the button clicked

---
+++ Modules/NotificationModule.js
@@ -0,0 +1,37 @@
+const NotificationModule = function(){
+
+    const notification = [
+        {date: new Date("2021-06-01"), restaurant:"Oslo", message:"Kjøleskap byttes"},
+        {date: new Date("2021-06-02"), restaurant:"Bergen", message:"Ny ansatt (daglig leder) Julie Julie starter"},
+        {date: new Date("2021-06-04"), restaurant:"Oslo", message:"Ny kampanje setter i gang (To for To Torsdag)"},
+        {date: new Date("2021-06-05"), restaurant:"Kristiansand", message:"Ansatt (kokk) Bob Bob slutter"},
+        {date: new Date("2021-06-05"), restaurant:"Oslo", message:"Sommervikar Trine Trine starter"},
+        {date: new Date("2021-06-08"), restaurant:"Oslo", message:"Kampanje Tacotorsdag slutter"},
+        {date: new Date("2021-06-08"), restaurant:"Stavanger", message:""},
+        {date: new Date("2021-06-13"), restaurant:"Stavanger", message:""},
+        {date: new Date("2021-06-16"), restaurant:"Oslo", message:""},
+        {date: new Date("2021-06-16"), restaurant:"Oslo", message:""}
+
+    ]
+
+    const getAllNotifications =()=> notification;
+    const getNotificationByRestaurant =(restaurant)=> notification.filter(notification => notification.restaurant === restaurant);
+    const getNotificationByDate =(date)=> notification.filter(notification => notification.date.toString === date);
+    const printNotificationItem =(notificationItem)=> {return `
+   
+        <div class="column is-full">
+            <div class="columns">
+                <div class="date-info-card column is-one-fifth card">
+                    ${notificationItem.date.toDateString()}
+                </div>
+                <div class="info-text-card column is-four-fifths card">
+                    ${notificationItem.message}
+                </div>
+            </div>
+        </div>
+        `} 
+
+        return{getAllNotifications, getNotificationByRestaurant, getNotificationByDate, printNotificationItem}
+}()
+
+export default NotificationModule;
--- Restauranter/restauranter.js
+++ Restauranter/restauranter.js
@@ -1,10 +1,53 @@
 import AnsattModule from "../Modules/AnsattModule.js";
+import NotificationModule from "../Modules/NotificationModule.js"
+import AddressModule from "../Modules/AddressModule.js"
 
+
+
+//HTML-elements
 var ansattCardContainer = document.getElementById("ansattCardContainer");
+var notificationCardContainer = document.getElementById("notificationCardContainer")
+var addressCardContainer = document.getElementById("AddressCardContainer");
+var restaurantButtons = document.querySelectorAll(".restaurant-button");
 
-var employees = AnsattModule.getEmployeeByRestaurant("Oslo");
 
+
+//variables
+let selectedLocation = "Oslo";
+updateWebpage(selectedLocation);
+
+function changeRestaurant(restaurantName){
+    console.log(restaurantName.value)
+}
+
+document.querySelectorAll(".restaurant-button").forEach(restaurant => {
+    restaurant.addEventListener("click", (r)=>{
+        selectedLocation = r.srcElement.value
+        updateWebpage(selectedLocation);
+        
+    })
+})
+
+
+function updateWebpage(locationName){
+
+//Module-arrays
+var employees = AnsattModule.getEmployeeByRestaurant(locationName);
+var notifications = NotificationModule.getNotificationByRestaurant(locationName)
+var address = AddressModule.getAddressByRestaurant(locationName);
+ansattCardContainer.innerHTML = "";
+notificationCardContainer.innerHTML = "";
+addressCardContainer.innerHTML = "";
+
+address.forEach(address => {
+    addressCardContainer.innerHTML += AddressModule.printAddressItem(address);
+})
 
 employees.forEach(employee => {
     ansattCardContainer.innerHTML += AnsattModule.printemployee(employee);
 });
+
+notifications.forEach(notification => {
+    notificationCardContainer.innerHTML += NotificationModule.printNotificationItem(notification);
+});
+}
