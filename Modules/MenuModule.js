918428b901c29a8c4ba20c4ee39def350b20d469
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2021-05-26 17:08:15 +0200

    Pictures
    La til bilder

--- Modules/MenuModule.js
+++ Modules/MenuModule.js
@@ -1,12 +1,12 @@
 const MenuModule = function () {
 
     const menu = [
-        { category: "Pizza", image: "pizza_00.jpg", name: "OsloPizza", price: 199.99, description: "pizza med tomatsaus, ost, kebabkjøtt og mais", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete ","melk ","nøtter"] },
-        { category: "Pizza", image: "pizza_01.jpg", name: "Hawaii", price: 149.99, description: "pizza med tomatsaus, ost, ananas, løk og kylling", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete ","melk"] },
-        { category: "Pizza", image: "pizza_02.jpg", name: "Pepperoni", price: 159.99, description: "pizza med tomatsaus, ost og pepperoni", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["melk ","nøtter"] },
-        { category: "Pizza", image: "pizza_03.jpg", name: "Skinke", price: 149.99, description: "pizza med tomatsaus, ost og skinke", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete ","melk"] },
-        { category: "Pizza", image: "pizza_04.jpg", name: "Maragrita", price: 129.99, description: "pizza med tomatsaus og ost", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete ","melk"] },
-        { category: "Pizza", image: "pizza_00.jpg", name: "Meat heaven", price: 299.99, description: "pizza med kebabkjøtt og ost", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete "] },
+        { category: "Pizza", image: "pizza_00.jpg", name: "OsloPizza", price: 199.99, description: "pizza med tomatsaus, ost, kebabkjøtt og mais", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete","melk","nøtter"] },
+        { category: "Pizza", image: "pizza_01.jpg", name: "Hawaii", price: 149.99, description: "pizza med tomatsaus, ost, ananas, løk og kylling", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete","melk"] },
+        { category: "Pizza", image: "pizza_02.jpg", name: "Pepperoni", price: 159.99, description: "pizza med tomatsaus, ost og pepperoni", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["melk","nøtter"] },
+        { category: "Pizza", image: "pizza_03.jpg", name: "Skinke", price: 149.99, description: "pizza med tomatsaus, ost og skinke", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete","melk"] },
+        { category: "Pizza", image: "pizza_04.jpg", name: "Maragrita", price: 129.99, description: "pizza med tomatsaus og ost", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete","melk"] },
+        { category: "Pizza", image: "pizza_00.jpg", name: "Meat heaven", price: 299.99, description: "pizza med kebabkjøtt og ost", ingredients: ["kebabkjøtt", "ost", "tomatsaus"], allergies: ["hvete"] },
         { category: "Drink", image: "cola.jpg", name: "0.5L Coca Cola", price: 29.99, description: "Coca Cola", ingredients: [], allergies: [] },
         { category: "Drink", image: "zero.jpg", name: "0.5L Coca Cola Zero", price: 29.99, description: "Coca Cola Zero", ingredients: [], allergies: [] },
         { category: "Drink", image: "sjoko.jpg", name: "0.5L Sjokomelk", price: 29.99, description: "Hjemmelaget sjokolademelk", ingredients: [], allergies: [] },
