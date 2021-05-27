1759704702a687b598c4125f8e8a979e91988f7e
mariestigen <marie.stigen@hotmail.com>
2021-05-27 12:24:19 +0200

    Koblet Sales-html til JS og Modul

--- Modules/SalesModule.js
+++ Modules/SalesModule.js
@@ -1,7 +1,10 @@
 const SalesModule = function(){
 
     const sales = [
-        {date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93,},
+        {date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
+        {date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
+        {date: new Date("2021-05-10"), restaurant: "Bergen", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
+        {date: new Date("2021-05-10"), restaurant: "Kristiansand", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
     ]
 
     const getAllSales =()=> sales;
@@ -9,6 +12,7 @@ const SalesModule = function(){
     const getSalesByRestaurant =(restaurant)=> sales.filter(sales => sales.restaurant.toLowerCase() === restaurant.toLowerCase());
 
     const getSalesByEarnings =(earnings)=> meny.filter(sales => sales.earnings === ParseInt(earnings));
+
 
     const addSale = (newDate, newRestaurant, newTransactions, newEarnings) => sales.push({date: new Date(newDate), restaurant:newRestaurant, transactions:newTransactions, earnings:newEarnings})
     const printSalesItem =(salesItem)=> {return `
@@ -18,4 +22,4 @@ const SalesModule = function(){
         return{getAllSales, getSalesByDate, getSalesByEarnings, printSalesItem, getSalesByRestaurant, addSale}
 }()
 
-export default MenyModule;
+export default SalesModule;
--- Sales/sales.js
+++ Sales/sales.js
@@ -1,52 +1,109 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
+import SalesModule from "../Modules/SalesModule.js"
+
+
+//HTML-Variables
+const salesCardContainer = document.getElementById("salesCardContainer");
+const fromDateSales = document.getElementById("fromDateSales");
+const toDateSales = document.getElementById("toDateSales");
+const salesSearchBtn = document.getElementById("salesSearchBtn");
+
+
+
+const allSales = SalesModule.getAllSales();
+const restaurantWithSale = new Set()
+
+allSales.forEach(sales => {
+    restaurantWithSale.add(sales.restaurant)
 });
-const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  };
+
+restaurantWithSale.forEach(restaurant => {
+    salesCardContainer.innerHTML += `<div class="column is-one-quarter">
+    <div class="card">
+        <div class="card-header title card-title-padding">
+            ${restaurant.toUpperCase()}
+        </div>					
+    </div>
+
+    <div class="columns is-multiline">
+        <div class="column is-full">
+            <div class="card height-200 center-text">
+                hello
+            </div>
+        </div>
+        <div class="column is-full">
+            <div class="card height-100 center-text">
+                hello
+            </div>
+        </div>
+        <div class="column is-full">
+            <div class="card height-50 center-text">
+                hello
+            </div>
+        </div>
+        <div class="column is-full">
+            <div class="card height-100 center-text">
+                hello
+            </div>
+        </div>
+        <div class="column is-full">
+            <div class="card height-50 center-text">
+                hello
+            </div>
+        </div>
+    </div>
+</div>`
+});
+
+salesSearchBtn.onclick =()=> updateSalesInfo();
+
+function updateSalesInfo(){
+    const fromDateValue = new Date(fromDateSales.value);
+    const toDateValue = new Date(toDateSales.value);
+
+    allSales.forEach(sale => {
+        if(sale.date > fromDateValue && sale.date < toDateValue){
+            console.log("IN RANGE")
+        }
+        else{
+            console.log("NOT IN RANGE")
+        }
+    })
+}
+
+/*{date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},*/
+
+  /*<div class="column is-one-quarter">
+				<div class="card">
+					<div class="card-header title card-title-padding">
+						OSLO
+					</div>					
+				</div>
+
+				<div class="columns is-multiline">
+					<div class="column is-full">
+						<div class="card height-200 center-text">
+							hello
+						</div>
+					</div>
+					<div class="column is-full">
+						<div class="card height-100 center-text">
+							hello
+						</div>
+					</div>
+					<div class="column is-full">
+						<div class="card height-50 center-text">
+							hello
+						</div>
+					</div>
+					<div class="column is-full">
+						<div class="card height-100 center-text">
+							hello
+						</div>
+					</div>
+					<div class="column is-full">
+						<div class="card height-50 center-text">
+							hello
+						</div>
+					</div>
+				</div>
+			</div>*/
