dafb70a1ff4f4b1ea94f0bc64a170e9a9552cb3f
mariestigen <marie.stigen@hotmail.com>
2021-05-27 19:48:03 +0200

    Added dynamic sales page

--- Modules/SalesModule.js
+++ Modules/SalesModule.js
@@ -1,10 +1,13 @@
 const SalesModule = function(){
 
     const sales = [
-        {date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
-        {date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
-        {date: new Date("2021-05-10"), restaurant: "Bergen", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
-        {date: new Date("2021-05-10"), restaurant: "Kristiansand", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},
+        {date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{Hawaii:12, Pepperoni:14, OsloPizza:11, Skinke:2}, earnings: 20394.93},
+        {date: new Date("2021-05-11"), restaurant: "Oslo", transactions: 123, itemsSold:{Hawaii:12, Margarita:14, Pepperoni:11, Margarita:2}, earnings: 40234.93},
+        {date: new Date("2021-05-15"), restaurant: "Oslo", transactions: 13, itemsSold:{Hawaii:2, Margarita:3, Pepperoni:1, "0.5L Coca Cola":2}, earnings: 20394.93},
+        {date: new Date("2021-06-01"), restaurant: "Bergen", transactions: 19, itemsSold:{OsloPizza:12, "Meat heaven":14, Pepperoni:11, "0.5L Sjokomelk":203}, earnings: 93423.93},
+        {date: new Date("2021-06-01"), restaurant: "Kristiansand", transactions: 1505, itemsSold:{OsloPizza:12, Hvitløksdressing:14, Pepperoni:11, cocacola:2}, earnings: 302312.93},
+        {date: new Date("2021-06-01"), restaurant: "Oslo", transactions: 155, itemsSold:{Hawaii:12, Skinke:14, Pepperoni:11, Oregano:2}, earnings: 20394.93},
+        {date: new Date("2021-06-01"), restaurant: "Stavanger", transactions: 155, itemsSold:{OsloPizza:12, Pepperoni:14, Hawaii:11, Skinke:2}, earnings: 20394.93},
     ]
 
     const getAllSales =()=> sales;
--- Sales/sales.js
+++ Sales/sales.js
@@ -1,4 +1,5 @@
 import SalesModule from "../Modules/SalesModule.js"
+import MenuModule from "../Modules/MenuModule.js"
 
 
 //HTML-Variables
@@ -7,68 +8,120 @@ const fromDateSales = document.getElementById("fromDateSales");
 const toDateSales = document.getElementById("toDateSales");
 const salesSearchBtn = document.getElementById("salesSearchBtn");
 
-
-
+//JS-variables
 const allSales = SalesModule.getAllSales();
+const allMenu = MenuModule.getAllMenuItems();
 const restaurantWithSale = new Set()
+const menuItems = new Set();
+let salesInfo = updateSalesInfo();
 
 allSales.forEach(sales => {
     restaurantWithSale.add(sales.restaurant)
 });
 
-restaurantWithSale.forEach(restaurant => {
-    salesCardContainer.innerHTML += `<div class="column is-one-quarter">
-    <div class="card">
-        <div class="card-header title card-title-padding">
-            ${restaurant.toUpperCase()}
-        </div>					
-    </div>
+allMenu.forEach(item => {
+    menuItems.add(item.name)
+})
 
-    <div class="columns is-multiline">
-        <div class="column is-full">
-            <div class="card height-200 center-text">
-                hello
-            </div>
-        </div>
-        <div class="column is-full">
-            <div class="card height-100 center-text">
-                hello
-            </div>
-        </div>
-        <div class="column is-full">
-            <div class="card height-50 center-text">
-                hello
-            </div>
-        </div>
-        <div class="column is-full">
-            <div class="card height-100 center-text">
-                hello
-            </div>
-        </div>
-        <div class="column is-full">
-            <div class="card height-50 center-text">
-                hello
-            </div>
-        </div>
-    </div>
-</div>`
-});
+updateSalesPage();
 
-salesSearchBtn.onclick =()=> updateSalesInfo();
+function printHTMLCards(array){
+    salesCardContainer.innerHTML = "";
+
+    restaurantWithSale.forEach(restaurant => {
+        salesCardContainer.innerHTML += `<div class="column is-one-quarter">
+        <div class="card">
+            <div class="card-header title card-title-padding">
+                ${restaurant.toUpperCase()}
+            </div>					
+        </div>
+    
+        <div class="columns is-multiline">
+            <div class="column is-full">
+                <div class="card height-200 center-text">
+                    ${returnCorrectObject(array, restaurant).restaurant}
+                </div>
+            </div>
+            <div class="column is-full">
+                <div class="card height-200 center-text">
+                ${printObject(returnCorrectObject(array, restaurant).itemsSold)}
+                </div>
+            </div>
+            <div class="column is-full">
+                <div class="card height-50 center-text">
+                ${returnCorrectObject(array, restaurant).transactions}
+                </div>
+            </div>
+            <div class="column is-full">
+                <div class="card height-100 center-text">
+                ${returnCorrectObject(array, restaurant).earnings}
+                </div>
+            </div>
+            <div class="column is-full">
+                <div class="card height-50 center-text">
+                    hello
+                </div>
+            </div>
+        </div>
+    </div>`
+    });
+}
+
+
+salesSearchBtn.onclick = updateSalesPage
+
+function updateSalesPage(){
+    printHTMLCards(updateSalesInfo());
+}
+
+function printObject(obj){
+    let output = ""
+    Object.entries(obj).filter((arr) => arr[1] != 0).forEach(([a,b]) => {output+= `${a} : ${b} <br>`})
+    return output
+}
+
+function returnCorrectObject(arr, restaurant){
+    let selectedObj = {}
+    arr.forEach(obj => {if (obj.restaurant.toLowerCase() === restaurant.toLowerCase()){
+        selectedObj = obj;
+        console.log(obj)
+    }})
+    return selectedObj;
+}
 
 function updateSalesInfo(){
     const fromDateValue = new Date(fromDateSales.value);
     const toDateValue = new Date(toDateSales.value);
+    let salesInfo = []
 
-    allSales.forEach(sale => {
-        if(sale.date > fromDateValue && sale.date < toDateValue){
-            console.log("IN RANGE")
-        }
-        else{
-            console.log("NOT IN RANGE")
-        }
+    restaurantWithSale.forEach(restaurant => {
+
+        let initItemSold = {}
+        menuItems.forEach(item => {
+            initItemSold[item] = 0
+        })
+
+        let tempObj = {restaurant, transactions:0, itemsSold:initItemSold, earnings:0}
+
+        allSales.forEach(sale => {
+            if(sale.date >= fromDateValue && sale.date <= toDateValue && sale.restaurant == restaurant){
+                tempObj.transactions += sale.transactions;
+                tempObj.earnings += sale.earnings;
+                var items = Object.keys(sale.itemsSold);
+                items.forEach(key => {
+                    if(Object.keys(tempObj.itemsSold).indexOf(key)!=-1){
+                        tempObj.itemsSold[key] += sale.itemsSold[key]
+                    }
+                })              
+            }
+        })
+
+        salesInfo.push(tempObj)  
     })
+ return salesInfo;
 }
+
+
 
 /*{date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},*/
 
