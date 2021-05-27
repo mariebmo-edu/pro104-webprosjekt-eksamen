45cd126cdb3b3608113aca72d8af26acf9386393
mariestigen <marie.stigen@hotmail.com>
2021-05-27 21:16:57 +0200

    Added comments to restaurants and sales JS

--- Restauranter/restauranter.js
+++ Restauranter/restauranter.js
@@ -19,26 +19,25 @@ const popUpContainer = document.getElementById("popUpContainer");
 let selectedLocation = "Oslo";
 
 
+//legger til butikk-knappene, samt oppdaterer nettsiden
 addStoreButtons();
 updateWebpage(selectedLocation);
 
-function changeRestaurant(restaurantName){
-    console.log(restaurantName.value)
-}
 
-
-
-
+//legger til butikk-knappene
 function addStoreButtons(){
     storeButtonContainer.innerHTML = ""
 
+    //henter alle lokasjonene fra adresse-modulen og legger de inn i HTML
     var addedLocations = AddressModule.getAllAddresses();
     addedLocations.forEach(location => {
         storeButtonContainer.innerHTML += ` <button class="restaurant-button" value="${location.restaurant.toLowerCase()}" alt="knapp som tar deg til restaurantsiden for ${location.restaurant}">${location.restaurant.toUpperCase()}</button>`
     })
 
+    //legger til "legg til restaurant"-knapp på slutten
     storeButtonContainer.innerHTML += ` <button class="restaurant-button" value="add-restaurant" alt="knapp som tar deg til "legg til restaurant"><i class="bi bi-plus-lg"></i>`
 
+    //knappen legger til onclick-listener for å se om man skal endre side. Hvis den klikkes på legges valuen til knappen (navnet) inn i updateWebpage
     document.querySelectorAll(".restaurant-button").forEach(restaurant => {
         restaurant.addEventListener("click", (r)=>{
             selectedLocation = r.srcElement.value
@@ -48,46 +47,51 @@ function addStoreButtons(){
     })
 }
 
+//oppdaterer nettsiden
 function updateWebpage(locationName){
 
+    //legger riktig underside til toppteksten på siden
     topAndSideBar.innerHTML = LayoutModule.printBaseLayout(`Restauranter > ${locationName}`);
 
 
+    //hvis locationName er "add-restaurant" skal den kjøre "addRestaurant()"-funksjonen
     if(locationName === "add-restaurant"){
         addRestaurant()
         console.log("clicking add restaurant");
     }else{
 
-        //Module-arrays
-
-        console.log(locationName);
-
-
+        //Module-arrays, oppdateres basert på valgt restaurant
         var employees = AnsattModule.getEmployeeByRestaurant(locationName);
         var notifications = NotificationModule.getNotificationByRestaurant(locationName)
         var address = AddressModule.getAddressByRestaurant(locationName);
 
+        //nullstiller HTML
         ansattCardContainer.innerHTML = "";
         notificationCardContainer.innerHTML = "";
         addressCardContainer.innerHTML = "";
 
+        //legger til adresse
         address.forEach(address => {
             addressCardContainer.innerHTML += AddressModule.printAddressItem(address);
         })
 
+        //legger til ansatte
         employees.forEach(employee => {
             ansattCardContainer.innerHTML += AnsattModule.printemployee(employee);
         });
 
+        //legger til notifications
         notifications.forEach(notification => {
             notificationCardContainer.innerHTML += NotificationModule.printNotificationItem(notification);
         });
 
+        //oppdaterer knapper
         addStoreButtons();
 
     }
 }
 
+//legger til ny restaurant
 function addRestaurant(){
 
     popUpContainer.innerHTML = newRestaurantPopup();
@@ -101,7 +105,7 @@ function addRestaurant(){
 }
 
 
-
+//pop-up som genereres når knappen trykkes på
 function newRestaurantPopup(){
 
     return `<div class="card pop-up-card">
@@ -121,6 +125,7 @@ function newRestaurantPopup(){
    
 }
 
+//legger til ny adresse i adresse-modulen basert på input og oppdaterer nettsiden
 function addNewAddress(){
 
     let locationNamePopUp = document.getElementById("locationNamePopUpInput").value;
--- Sales/sales.js
+++ Sales/sales.js
@@ -15,20 +15,34 @@ const allSales = SalesModule.getAllSales();
 const allMenu = MenuModule.getAllMenuItems();
 const restaurantWithSale = new Set()
 const menuItems = new Set();
-let salesInfo = updateSalesInfo();
 
+//Genererer topp- og sidebar med teksten "salg"
 topAndSideBar.innerHTML = LayoutModule.printBaseLayout("Salg");
 
+
+//For hvert element i "sales", så legges restaurantnavnet i en set-list
 allSales.forEach(sales => {
     restaurantWithSale.add(sales.restaurant)
 });
 
+//for hvert element i "menu" legges hvert item-navn i menuItems set-listen
 allMenu.forEach(item => {
     menuItems.add(item.name)
 })
 
+//oppdaterer salgsiden
 updateSalesPage();
 
+//når man klikker på knappen oppdateres siden.
+salesSearchBtn.onclick = updateSalesPage
+
+
+function updateSalesPage(){
+    printHTMLCards(updateSalesInfo());
+}
+
+
+//printer HTML-layoutet til nettsiden, tar inn et array den bruker for de forskjellige elementene.
 function printHTMLCards(array){
     salesCardContainer.innerHTML = "";
 
@@ -73,18 +87,14 @@ function printHTMLCards(array){
 }
 
 
-salesSearchBtn.onclick = updateSalesPage
-
-function updateSalesPage(){
-    printHTMLCards(updateSalesInfo());
-}
-
+//returnerer et objekt som en string
 function printObject(obj){
     let output = ""
     Object.entries(obj).filter((arr) => arr[1] != 0).forEach(([a,b]) => {output+= `${a} : ${b} <br>`})
     return output
 }
 
+//returnerer riktig objekt basert på restaurantnavnet
 function returnCorrectObject(arr, restaurant){
     let selectedObj = {}
     arr.forEach(obj => {if (obj.restaurant.toLowerCase() === restaurant.toLowerCase()){
@@ -94,25 +104,34 @@ function returnCorrectObject(arr, restaurant){
     return selectedObj;
 }
 
+//oppdaterer salgsinformasjonen basert på til og fra-dato
 function updateSalesInfo(){
     const fromDateValue = new Date(fromDateSales.value);
     const toDateValue = new Date(toDateSales.value);
     let salesInfo = []
 
+    //kjøres for hver restaurant i restaurant-arrayet
     restaurantWithSale.forEach(restaurant => {
 
+        //legger til meny-items som base i et tomt objekt
         let initItemSold = {}
         menuItems.forEach(item => {
             initItemSold[item] = 0
         })
 
+        //laget et tomt objekt med nullstilte verdier 
         let tempObj = {restaurant, transactions:0, itemsSold:initItemSold, earnings:0}
 
+        //for hvert salg sjekkes det om det er innenfor valgte datoer, og i den valgte restauranten. Hvis den er det legges plusses verdiene til det tomme arrayet
         allSales.forEach(sale => {
             if(sale.date >= fromDateValue && sale.date <= toDateValue && sale.restaurant == restaurant){
                 tempObj.transactions += sale.transactions;
                 tempObj.earnings += sale.earnings;
+
+                //lager nytt array basert på itemsSold i salget.
                 var items = Object.keys(sale.itemsSold);
+
+                //for hver item i arrayet, ser man om temp-objektets nøkkel eksisterer, og hvis det gjør det plusses verdien på verdien som er i salget.
                 items.forEach(key => {
                     if(Object.keys(tempObj.itemsSold).indexOf(key)!=-1){
                         tempObj.itemsSold[key] += sale.itemsSold[key]
@@ -121,47 +140,9 @@ function updateSalesInfo(){
             }
         })
 
+        //legger til tempObj i salesInfo-arrayet
         salesInfo.push(tempObj)  
     })
+    //returnerer salesInfo-arrayet med salgsdata for alle transaksjoner innen gitt tidsrom
  return salesInfo;
 }
-
-
-
-/*{date: new Date("2021-05-10"), restaurant: "Oslo", transactions: 155, itemsSold:{oslopizza:12, kebabpizza:14, pepperonipizza:11, cocacola:2}, earnings: 20394.93},*/
-
-  /*<div class="column is-one-quarter">
-				<div class="card">
-					<div class="card-header title card-title-padding">
-						OSLO
-					</div>					
-				</div>
-
-				<div class="columns is-multiline">
-					<div class="column is-full">
-						<div class="card height-200 center-text">
-							hello
-						</div>
-					</div>
-					<div class="column is-full">
-						<div class="card height-100 center-text">
-							hello
-						</div>
-					</div>
-					<div class="column is-full">
-						<div class="card height-50 center-text">
-							hello
-						</div>
-					</div>
-					<div class="column is-full">
-						<div class="card height-100 center-text">
-							hello
-						</div>
-					</div>
-					<div class="column is-full">
-						<div class="card height-50 center-text">
-							hello
-						</div>
-					</div>
-				</div>
-			</div>*/
