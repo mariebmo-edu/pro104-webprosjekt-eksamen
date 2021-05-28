66cd36d27655cccf84bc7f8bdd7e82306b7dafee
ToMrArcher <syvertcollege@gmail.com>
2021-05-28 13:15:01 +0200

    added dashboard fixes and new layout. Its not cool to Marie's liking

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -16,9 +16,25 @@ leftsideHTML += `<div class="card restaurants mb-5">
                         <div class="card-header card-title-padding title yellow-background">
                             Notifications!
                         </div>
-                        <div class="card-content border-between-grey columns is-multiline">
-                            <div class="card column is-12">
-                                <div class="card-content">
+                </div>
+                <div class="card">
+                    <div class="card-header card-title-padding title yellow-background">Notes</div>
+                    <div class="card-content">
+                                <div class="content">
+                                `;
+for (var i = 0; i < 5; i++) {
+    leftsideHTML += `<h2>${notifications[i].restaurant}</h2>
+    <p>${notifications[i].message}</p>
+    <p>${notifications[i].date.getDate()}/${notifications[i].date.getMonth()}/${notifications[i].date.getFullYear()}</p>`;
+}
+leftsideHTML += `
+                                </div>
+                            </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header card-title-padding title yellow-background">Ansatte</div>
+                    <div class="card-content">
                                     <div class="content">
                                         <h5 class="subtitle">Nyansatt</h5>
                                         <p>Julie jul</p>
@@ -32,26 +48,18 @@ leftsideHTML += `<div class="card restaurants mb-5">
                                         <p>Julie jul</p>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="column is-12">
-                                <div class="content">
-                                `;
-for (var i = 0; i < 5; i++) {
-    leftsideHTML += `<h2>${notifications[i].restaurant}</h2>
-                                                                                    <p>${notifications[i].message}</p>`;
-}
-leftsideHTML += `
-                                </div>
-                            </div>
-                            <div class="column is-12">
-                                <div class="content">
-                                    <h5 class="subtitle">Nåværende kampanjer</h5>
-                                    <p>Pizza og kos 199.-</p>
-                                    <p>Netflix and chilli 149.-</p>
-                                </div>
-                            </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header card-title-padding title yellow-background">Kampanjer</div>
+                    <div class="card-content">
+                        <div class="content">
+                            <h5 class="subtitle">Nåværende kampanjer</h5>
+                            <p>Pizza og kos 199.-</p>
+                            <p>Netflix and chilli 149.-</p>
                         </div>
-                    </div>`;
+                    </div>
+                </div>`;
 
 
 document.getElementById("left").innerHTML = leftsideHTML;
